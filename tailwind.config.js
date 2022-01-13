0a0a24c0911b43165be5936ea1afc1c5a0b3e62e
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2022-01-13 11:23:49 -0500

    removed theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,7 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './pages/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        blue: '#1D3FB5',
-        purple: '#7715C3',
-        white: '#fff'
-      },
-      fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
-        body: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif'],
-        header: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif'],
-      },
-      boxShadow: {
-        bold: '0 1px 2px 0 rgba(0, 0, 0, 0.30)',
-        black: '4px 4px 0 8px rgba(0, 0, 0, 1)',
-        black2: '2px 2px 0 4px rgba(0, 0, 0, 1)',
-        bottom: '0 2px 2px -2px rgba(0, 0, 0, 1)',
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
