eae37329ae721a0c822cc7aff7ebd91e791cb956
amudryk <mudrykadam@gmail.com>
2022-01-14 10:19:29 -0700

    basic expanded card css

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import Navbar from '../src/components/navbar'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container} class="min-h-screen min-w-screen overflow-hidden ">
+    <div class="min-h-screen min-w-screen overflow-hidden ">
       <Head>
         <title>EdRover</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,7 +12,7 @@ export default function Home() {
       <Navbar selected={0}/>
     
       <div className="bg-gradient-to-b from-blue-700 to-purple-800 text-center mt-20 pt-20 -z-1">
-        <header className="font-header text-white text-3xl mx-5">
+        <header className="text-white text-3xl mx-5">
           Discover the perfect higher education plan for you.
         </header>
         <Image src="/home_image.png" width="1052" height="646"/>
